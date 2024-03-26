7dc647400de79a00aaa8bafeecc8629917af0882
BrondonC <165023041+BrondonC@users.noreply.github.com>
2024-03-26 21:35:31 +0800

    Add files via upload

---
+++ PacmanSemesterProject/Pacman.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JFrame;
+
+import Pacman.Model;
+
+public class Pacman extends JFrame{
+
+	public Pacman() {
+		add(new Model());
+	}
+	
+	
+	public static void main(String[] args) {
+		Pacman pac = new Pacman();
+		pac.setVisible(true);
+		pac.setTitle("Pacman");
+		pac.setSize(500,550);
+		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pac.setLocationRelativeTo(null);
+		
+	}
+
+}
